2782efc94180ac1012c717d9745320b0e20d38f8
Chris Nelson <c4nelson@eng.ucsd.edu>
2015-03-31 19:18:50 +0000

    Added cython as dependency
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/pyinstall@20597 2cf53259-273a-af40-bda6-69d31023b8ec

--- pipinstall.sh
+++ pipinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-dependencies="lxml pypng beautifulsoup4 requests svgwrite Mako clang bintrees numpy jinja2 Sphinx asciitree"
+dependencies="cython lxml pypng beautifulsoup4 requests svgwrite Mako clang bintrees numpy jinja2 Sphinx asciitree"
 
 site_packages=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
 if [ -w "$site_packages" ] 
