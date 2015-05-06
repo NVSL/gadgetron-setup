f7d0ce9ac9bf20571674e73148f9e733b22ad64f
Steven Swanson <swanson@cs.ucsd.edu>
2015-05-06 06:03:54 +0000

    More cleanup for distribution.
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/pyinstall@20861 2cf53259-273a-af40-bda6-69d31023b8ec

--- pipinstall.sh
+++ pipinstall.sh
@@ -2,7 +2,13 @@
 
 dependencies="cython lxml pypng beautifulsoup4 requests svgwrite Mako clang bintrees numpy jinja2 Sphinx asciitree"
 
+if [ "$USER." = "swanson." ]; then
+    virtualenv $GADGETRON_ROOT/Python
+    pip install $dependencies
+fi
+
 site_packages=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
+
 if [ -w "$site_packages" ] 
 then
     pip install $dependencies
