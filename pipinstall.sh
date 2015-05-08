1a2f0d9f29d1db9ff8ae3eaa00e0b179b4af07d7
Steven Swanson <swanson@cs.ucsd.edu>
2015-05-08 02:22:30 +0000

    Complete reorganization to match setuptool standard practice.
    Setuptools is really pretty awesome.  Especially 'develop' mode
    
    Also, if you 
    
    export USE_VENV=yes
    
    Everything gadgetron related will get its own python directory (in Gadgets/Python/).  This will also let you use develop mode.
    
    
    
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/pyinstall@20891 2cf53259-273a-af40-bda6-69d31023b8ec

--- pipinstall.sh
+++ pipinstall.sh
@@ -2,9 +2,11 @@
 
 dependencies="cython lxml pypng beautifulsoup4 requests svgwrite Mako clang bintrees numpy jinja2 Sphinx asciitree"
 
-if [ "$USER." = "swanson." ]; then
+if [ "$USE_VENV." = "yes." ]; then
+    echo Using a virtual environment
     virtualenv $GADGETRON_ROOT/Python
     pip install $dependencies
+    exit;
 fi
 
 site_packages=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
