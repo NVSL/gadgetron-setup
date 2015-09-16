5516c319987669e81519e7c21e8327bab6356fb7
Steven Swanson <swanson@cs.ucsd.edu>
2015-09-16 06:26:15 +0000

    documentation.
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/pyinstall@22231 2cf53259-273a-af40-bda6-69d31023b8ec

--- svn2git.sh
+++ svn2git.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-DIR=$1
-GIT=$2
-shift
-shift
+DIR=$1  # Directory in our svn you want to import
+GIT=$2  # name of git repo on our gitlab server (you need to create this ahead of time. it should be empty)
 
 echo Checking out $1 from svn, and moving it to git@146.148.85.41:$USER/$GIT.git
 echo If git@146.148.85.41:$USER/$GIT.git does not exist, this will fail.
