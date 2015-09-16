de8e72b815d2f61053eb459ada20b023d5e541c6
Steven Swanson <swanson@cs.ucsd.edu>
2015-09-16 06:19:30 +0000

    fix terrible bug
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/pyinstall@22229 2cf53259-273a-af40-bda6-69d31023b8ec

--- gitinstall.sh
+++ gitinstall.sh
@@ -2,7 +2,7 @@
 
 # Envisioned workload:
 #  1.  Create a new development direcotry: 'mkdir Gadgetron'
-#  2.  git clone <git-url> to get contents of what is now Tools/pyinstall/
+#  2.  git clone git@146.148.85.41:swanson/test-pyinstall.git
 #  3.  execute 'pyinstall/gitinstall.sh'
 #  4.  Update your PATH (this script prints out the command you should execute)
 
@@ -24,7 +24,7 @@ VENV=$(cd $GADGETRON_ROOT/python_venv; pwd -P) # get the canonical path
 
 PATH=$VENV/bin:$PATH
 
-for p in $gadgetron_packages; do
+for p in $packages; do
     git clone http://146.148.85.41/swanson/${p}.git
     (cd $p; python setup.py develop)
 done
