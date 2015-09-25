88f031ce555aebfe5fe756ad426ee237eb52910f
Steven Swanson <swanson@cs.ucsd.edu>
2015-09-25 06:34:19 +0000

    tools for migrating to git
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/pyinstall@22402 2cf53259-273a-af40-bda6-69d31023b8ec

--- gitinstall.sh
+++ gitinstall.sh
@@ -15,18 +15,26 @@ if [ ".$GADGETRON_ROOT" = "." ]; then
     GADGETRON_ROOT=$PWD
 fi
 
-VENV=$GADGETRON_ROOT/python_venv
+if [ ".$PYTHON_ENV" = "." ]; then
+    PYTHON_ENV=$GADGETRON_ROOT/python_venv
+fi
+
+
+if ! [ -e $PYTHON_ENV/bin/python ]; then
+    rm -rf $PYTHON_ENV
+    virtualenv $PYTHON_ENV
+fi
+
+PYTHON_ENV=$(cd $GADGETRON_ROOT/python_venv; pwd -P) # get the canonical path
+
+PATH=$PYTHON_ENV/bin:$PATH
 
 packages="test-Swoop test-Koala2"
-
-virtualenv $VENV
-VENV=$(cd $GADGETRON_ROOT/python_venv; pwd -P) # get the canonical path
-
-PATH=$VENV/bin:$PATH
 
 for p in $packages; do
     git clone ssh://git@146.148.85.41/swanson/${p}.git
     (cd $p; python setup.py develop)
 done
 
-echo PATH=$VENV/bin:\$PATH
+echo PATH=$PYTHON_ENV/bin:\$PATH
+
--- svn2git.sh
+++ svn2git.sh
@@ -3,10 +3,20 @@
 DIR=$1  # Directory in our svn you want to import
 GIT=$2  # name of git repo on our gitlab server (you need to create this ahead of time. it should be empty)
 
-echo Checking out $1 from svn, and moving it to git@146.148.85.41:$USER/$GIT.git
-echo If git@146.148.85.41:$USER/$GIT.git does not exist, this will fail.
+echo $DIR
+echo $GIT
 
-git svn clone --authors-file=names.mapped svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/$DIR
-cd $DIR
-git remote add origin git@146.148.85.41:$USER/$GIT.git
-git push -u origin master
+if [ ".$DIR" = "." -o ".$GIT" = "." ]; then
+   echo "usage: svn2git.sh <path relative to svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets> <GIT repo name>"
+   exit 0
+fi 
+
+GITREPO=git@github.com:NVSL/$2.git
+
+echo Checking out svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/$1 from svn, and moving it to $GETREPO
+echo If $GITREPO does not exist, this will fail.
+
+echo git svn clone --authors-file=$GADGETRON_ROOT/Tools/pyinstall/names.mapped svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/$DIR
+echo cd $(echo $DIR| perl -ne '@a =split("/"); print "$a[-1]"')
+echo git remote add origin $GITREPO
+echo git push -u origin master
