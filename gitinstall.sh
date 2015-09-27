5e8c4284dc17cc910226ef431fa839d73421503b
Steven Swanson <swanson@cs.ucsd.edu>
2015-09-27 23:20:06 +0000

    add another git repo
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/pyinstall@22444 2cf53259-273a-af40-bda6-69d31023b8ec

--- gitinstall.sh
+++ gitinstall.sh
@@ -32,10 +32,10 @@ if [ ".$USE_VENV" != "." ]; then
     PATH=$PYTHON_ENV/bin:$PATH
 fi
 
-packages="BOBBuilder" #test-Swoop test-Koala2"
+packages="NVSL/BOBBuilder NVSL/gadgetron-vm-util" #test-Swoop test-Koala2"
 
 for p in $packages; do
-    git clone git@github.com:NVSL/$p.git
+    git clone git@github.com:$p.git
     (cd $p; make)
 done
 
