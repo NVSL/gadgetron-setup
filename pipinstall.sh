d1bacc7d26da88084296e4df63b2db548b5c610b
Steven Swanson <swanson@cs.ucsd.edu>
2015-09-27 19:08:19 +0000

    additional prerequisites
    git-svn-id: svn+ssh://bbfs-01.calit2.net/grw/Gordon/svn/trunk/Gadgets/Tools/pyinstall@22440 2cf53259-273a-af40-bda6-69d31023b8ec

--- pipinstall.sh
+++ pipinstall.sh
@@ -2,14 +2,17 @@
 
 # Install the non-python dependencies
 if command -v apt-get; then
-    sudo apt-get -y install cython python-lxml libspatialindex-dev python-pygame npm swig libpython-dev libcgal-dev
+    echo Updating system packages
+    sudo apt-get -y install python-pip libspatialindex-dev python-pygame libcgal-dev swig inkscape arduino curl nodejs npm subversion emacs git cython python-lxml libspatialindex-dev python-pygame npm swig libpython-dev libcgal-dev libxml2
 elif command -v brew; then
     brew install cgal swig
 elif command -v port; then
     sudo port install cgal swig swig-python
 fi
 
-dependencies="cython lxml pypng beautifulsoup4 requests svgwrite Mako clang bintrees numpy jinja2 Sphinx asciitree cgal-bindings"
+dependencies="cython lxml pypng beautifulsoup4 requests svgwrite Mako clang bintrees numpy jinja2 Sphinx asciitree cgal-bindings rtree pyparsing"
+
+sudo pip install virtualenv
 
 # this is necessary on unbuntu, it seems.
 export CPATH=/usr/include/libxml2
@@ -20,7 +23,6 @@ if [ "$USE_VENV." = "yes." ]; then
     pip install $dependencies
     pip install -r $GADGETRON_ROOT/Tools/CbC/requirements.txt
 else
-
 
     site_packages=`python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())"`
 
